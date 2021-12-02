e0539416c289b1ee30f8ffb52cc796cb6de80ebc
yasteen <justinchan135@gmail.com>
2021-12-01 22:26:49 -0800

    Add function to get next token string

--- parse.go
+++ parse.go
@@ -1,1 +1,29 @@
-package parse
+package main
+
+// Returns a string with the next token.
+func getNextTokenString(equation string, index int) (tokenString string, nextIndex int) {
+	tokenString = ""
+	for ; index < len(equation); index++ {
+		c := string(equation[index])
+		if c == " " {
+			continue
+		} else if c == TokenToString(RParen) || c == TokenToString(LParen) {
+			if tokenString == "" {
+				tokenString = c
+			}
+			index++
+			break
+		}
+		tokenString += c
+	}
+	return tokenString, nextIndex
+}
+
+func Parse(equation string, variables map[string]float64) {
+	tokens := []string{}
+	for i := 0; i < len(equation); {
+		token, nextIndex := getNextTokenString(equation, i)
+		tokens = append(tokens, token)
+		i = nextIndex
+	}
+}
