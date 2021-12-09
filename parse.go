3cbc53a5b8b268f8cd71fb6254e2f3e957d95a3f
yasteen <justinchan135@gmail.com>
2021-12-09 01:12:35 -0800

    Add isValidExpression with tests

--- parse.go
+++ parse.go
@@ -25,6 +25,34 @@ func getNextTokenString(expression string, index int) (tokenString string, nextI
 	return tokenString, index
 }
 
+func isValidExpression(tokens []string) bool {
+	if len(tokens) == 0 {
+		return true
+	}
+	prevToken, prevTokenType := StringToToken(tokens[0])
+	if len(tokens) == 1 {
+		return prevTokenType == Value
+	}
+	if prevTokenType == Operator || prevToken == RParen {
+		return false
+	}
+
+	for i := 1; i < len(tokens); i++ {
+		token, tokenType := StringToToken(tokens[i])
+		// Whether the previous token could be the end of an expression
+		prevIsEndable := prevTokenType == Value || prevToken == RParen
+		// Whether the current token can take place after an endable previous token
+		currFollows := tokenType == Operator || token == RParen
+		if (prevIsEndable && !currFollows) || (!prevIsEndable && currFollows) {
+			return false
+		}
+		prevToken = token
+		prevTokenType = tokenType
+	}
+
+	return prevTokenType == Value || prevToken == RParen
+}
+
 func Parse(equation string, variables map[string]float64) {
 	tokens := []string{}
 	for i := 0; i < len(equation); {
--- parse_internal_test.go
+++ parse_internal_test.go
@@ -1,6 +1,8 @@
 package parse
 
-import "testing"
+import (
+	"testing"
+)
 
 func testExpressionToTokens(input string, expected []string, t *testing.T) {
 	curStringIndex := 0
@@ -16,7 +18,33 @@ func testExpressionToTokens(input string, expected []string, t *testing.T) {
 		curStringIndex++
 	}
 }
+
+func testIsValidExpressionHelper(input []string, expected bool, t *testing.T) {
+	if isValidExpression(input) != expected {
+		str := ""
+		for _, s := range input {
+			str += s
+		}
+		t.Errorf("Test isValidExpression failed for %s.", str)
+	}
+}
+
 func TestGetNextTokenWithString(t *testing.T) {
 	testExpressionToTokens("sin(x)", []string{"sin", "(", "x", ")"}, t)
 	testExpressionToTokens("x * log((5 +3) / 2)", []string{"x", "*", "log", "(", "(", "5", "+", "3", ")", "/", "2", ")"}, t)
+}
+
+func TestIsValidExpression(t *testing.T) {
+	testIsValidExpressionHelper([]string{"x"}, true, t)
+	testIsValidExpressionHelper([]string{"(", "x", ")"}, true, t)
+	testIsValidExpressionHelper([]string{"sin", "x"}, true, t)
+	testIsValidExpressionHelper([]string{"1", "+", "3", "*", "sin", "y"}, true, t)
+	testIsValidExpressionHelper([]string{"(", "x", "^", "2", "-", "9", ")", "+", "x"}, true, t)
+
+	testIsValidExpressionHelper([]string{"("}, false, t)
+	testIsValidExpressionHelper([]string{")"}, false, t)
+	testIsValidExpressionHelper([]string{"+"}, false, t)
+	testIsValidExpressionHelper([]string{"sin"}, false, t)
+	testIsValidExpressionHelper([]string{"sin", "+", "x"}, false, t)
+	testIsValidExpressionHelper([]string{"4", "^"}, false, t)
 }
