a82c33a3834d983cef4d68aa526bff491b26bc98
Justin Chan <75227648+yasteen@users.noreply.github.com>
2021-12-12 14:27:40 -0800

    add function definitions for reals

--- types/mathgroups/real/real.go
+++ types/mathgroups/real/real.go
@@ -1,6 +1,7 @@
 package real
 
 import (
+	"math"
 	"strconv"
 
 	"github.com/yasteen/go-parse/types"
@@ -20,16 +21,50 @@ const (
 )
 
 var realTokenMap = map[types.Keyword]types.KeywordData{
-	Add:      {Symbol: "+", TokenType: types.Operator},
-	Subtract: {Symbol: "-", TokenType: types.Operator},
-	Multiply: {Symbol: "*", TokenType: types.Operator},
-	Divide:   {Symbol: "/", TokenType: types.Operator},
-	Power:    {Symbol: "^", TokenType: types.Operator},
-	Sin:      {Symbol: "sin", TokenType: types.SingleFunction},
-	Cos:      {Symbol: "cos", TokenType: types.SingleFunction},
-	Tan:      {Symbol: "tan", TokenType: types.SingleFunction},
-	Log:      {Symbol: "log", TokenType: types.SingleFunction},
-	Exp:      {Symbol: "exp", TokenType: types.SingleFunction},
+	Add: {Symbol: "+", TokenType: types.Operator,
+		Apply: func(params ...interface{}) interface{} {
+			return params[0].(float64) + params[1].(float64)
+		}},
+	Subtract: {Symbol: "-", TokenType: types.Operator,
+		Apply: func(params ...interface{}) interface{} {
+			return params[0].(float64) + params[1].(float64)
+		}},
+	Multiply: {Symbol: "*", TokenType: types.Operator,
+		Apply: func(params ...interface{}) interface{} {
+			return params[0].(float64) * params[1].(float64)
+		}},
+	Divide: {Symbol: "/", TokenType: types.Operator,
+		Apply: func(params ...interface{}) interface{} {
+			if params[1].(float64) == 0 {
+				// TODO: Handle division by 0
+				panic("Attempted division by 0")
+			}
+			return params[0].(float64) / params[1].(float64)
+		}},
+	Power: {Symbol: "^", TokenType: types.Operator,
+		Apply: func(params ...interface{}) interface{} {
+			return math.Pow(params[0].(float64), params[1].(float64))
+		}},
+	Sin: {Symbol: "sin", TokenType: types.SingleFunction,
+		Apply: func(params ...interface{}) interface{} {
+			return math.Sin(params[0].(float64))
+		}},
+	Cos: {Symbol: "cos", TokenType: types.SingleFunction,
+		Apply: func(params ...interface{}) interface{} {
+			return math.Cos(params[0].(float64))
+		}},
+	Tan: {Symbol: "tan", TokenType: types.SingleFunction,
+		Apply: func(params ...interface{}) interface{} {
+			return math.Tan(params[0].(float64))
+		}},
+	Log: {Symbol: "log", TokenType: types.SingleFunction,
+		Apply: func(params ...interface{}) interface{} {
+			return math.Log(params[0].(float64))
+		}},
+	Exp: {Symbol: "exp", TokenType: types.SingleFunction,
+		Apply: func(params ...interface{}) interface{} {
+			return math.Exp(params[0].(float64))
+		}},
 }
 
 var realStringToToken = map[string]types.Keyword{
