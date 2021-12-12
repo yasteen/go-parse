70fc5fb4f164b11a0e71800dbffa0daedb45ce2d
yasteen <justinchan135@gmail.com>
2021-12-11 16:26:08 -0800

    add tests for toPostfix

--- parse_internal_test.go
+++ parse_internal_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func testExpressionToTokens(input string, expected []string, t *testing.T) {
+func testGetNextTokenStringHelper(input string, expected []string, t *testing.T) {
 	curStringIndex := 0
 	for i := 0; i < len(input); {
 		var tokenString string
@@ -19,6 +19,10 @@ func testExpressionToTokens(input string, expected []string, t *testing.T) {
 		}
 		curStringIndex++
 	}
+}
+func TestGetNextTokenWithString(t *testing.T) {
+	testGetNextTokenStringHelper("sin(x)", []string{"sin", "(", "x", ")"}, t)
+	testGetNextTokenStringHelper("x * log((5 +3) / 2)", []string{"x", "*", "log", "(", "(", "5", "+", "3", ")", "/", "2", ")"}, t)
 }
 
 func testIsValidExpressionHelper(input []string, expected bool, t *testing.T) {
@@ -35,12 +39,6 @@ func testIsValidExpressionHelper(input []string, expected bool, t *testing.T) {
 		t.Error(errorString)
 	}
 }
-
-func TestGetNextTokenWithString(t *testing.T) {
-	testExpressionToTokens("sin(x)", []string{"sin", "(", "x", ")"}, t)
-	testExpressionToTokens("x * log((5 +3) / 2)", []string{"x", "*", "log", "(", "(", "5", "+", "3", ")", "/", "2", ")"}, t)
-}
-
 func TestIsValidExpression(t *testing.T) {
 	testIsValidExpressionHelper([]string{"x"}, true, t)
 	testIsValidExpressionHelper([]string{"(", "x", ")"}, true, t)
@@ -54,4 +52,33 @@ func TestIsValidExpression(t *testing.T) {
 	testIsValidExpressionHelper([]string{"sin"}, false, t)
 	testIsValidExpressionHelper([]string{"sin", "+", "x"}, false, t)
 	testIsValidExpressionHelper([]string{"4", "^"}, false, t)
+}
+
+func testToPostfixHelper(input []string, expected []string, unmatchedParen bool, t *testing.T) {
+	defer func() {
+		recover()
+	}()
+	output := toPostfix(input)
+
+	if unmatchedParen {
+		// Should not reach here if we expect a panic
+		t.Error("Failed to detect unmatched parentheses.")
+	} else {
+		if len(output) != len(expected) {
+			t.Error("Output and expected output length do not match. Expected", expected, "Produced", output)
+		}
+		for i := 0; i < len(expected); i++ {
+			if output[i] != expected[i] {
+				t.Error("Output and expected output do not match at index", i, "- Expected", expected[i], "Produced", output[i])
+			}
+		}
+	}
+}
+
+func TestToPostfixHelper(t *testing.T) {
+	testToPostfixHelper([]string{"x", "+", "4"}, []string{"x", "4", "+"}, false, t)
+	testToPostfixHelper([]string{"(", "(", "x", "+", "4", ")", "^", "5", ")"}, []string{"x", "4", "+", "5", "^"}, false, t)
+
+	testToPostfixHelper([]string{"(", "(", "x", "+", "4", "^", "5", ")"}, []string{}, true, t)
+	testToPostfixHelper([]string{"(", "x", "+", "4"}, []string{}, true, t)
 }
