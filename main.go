283359bf1e54b0c033071286220665b83c31ef22
Justin Chan <justinchan135@gmail.com>
2021-12-12 20:39:49 -0800

    add interface and pre-commit tests

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/yasteen/go-parse/types/mathgroups/real"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("What expression do you want to evaluate? (In terms of x)")
+	var expression string
+	if scanner.Scan() {
+		expression = scanner.Text()
+	}
+
+	fmt.Println("What value do you want x to be?")
+	var val float64
+	var err error
+	if scanner.Scan() {
+		val, err = strconv.ParseFloat(scanner.Text(), 64)
+	}
+	fmt.Println("Read value", val)
+
+	if err != nil {
+		panic("Bad values :(")
+	}
+
+	fmt.Println(real.MapValues(expression, *real.NewRealInterval(val, 2, val+1), "x"))
+}
